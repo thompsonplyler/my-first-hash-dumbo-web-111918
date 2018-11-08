d22661dcb015a6839ff5f086b3c72f4f366fb732
Thompson Plyler <nealok.g@gmail.com>
2018-11-08 13:29:07 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,10 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  your_hash = {
+          "name" = "Thompson"
+          "weight" = 210
+          "height" = 70
+          "league_rank" = "Silver I"
+  }
   
 end
 
