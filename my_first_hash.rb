732b17fafb36f3dccb6f5468f5e3d435c2e0d137
Thompson Plyler <nealok.g@gmail.com>
2018-11-08 13:36:42 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -39,8 +39,11 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
 
+                
+  # add 2 muskets to the shipping_manifest hash below
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
 
 
   # add 4 gun powder to the shipping_manifest hash below
